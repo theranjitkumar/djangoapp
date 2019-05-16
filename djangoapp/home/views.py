80d381552c26e4f26c81203ff64b70ab4a04b7f8
theranjitkumar <ranjit.kumar@mobileprogramming.net>
2019-05-16 19:02:52 +0530

    dinamic datta rendering

--- djangoapp/home/views.py
+++ djangoapp/home/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
+import datetime
 
 def index(request):
     # return HttpResponse("home")
-    return render(request, 'index.html', {})
+    now = datetime.datetime.now()
+    return render(request, 'index.html', {'current_date': now})
 def about(request):
     # return HttpResponse("about")
     return render(request, 'about.html', {}) 
--- djangoapp/post/views.py
+++ djangoapp/post/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+import json
+
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+posts = json.dumps(x)
 
 
 def post(request):
     # return HttpResponse("post")
-    return render(request, 'post.html', {})
+    # posts = Post.objects.all();
+    return render(request, 'post.html', {'posts': posts})
 
 def postDetails(request):
     # return HttpResponse("postDetails")
